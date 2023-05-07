b7c59b63d82c0bad8e985a26cee24f388831c07c
yoshimitakuto <129573171+yoshimitakuto@users.noreply.github.com>
2023-05-07 13:06:34 +0000

    [Finish]応用問題（正解！）

--- air_conditioner.rb
+++ air_conditioner.rb
@@ -2,6 +2,7 @@ input_lines = readlines
 
 room = input_lines[0].to_i
 air_conditioner = input_lines[1].to_i
+air_volume = input_lines[2].to_i
 
 temperature_diff = (room - air_conditioner).abs
 
@@ -15,4 +16,52 @@ elsif temperature_diff >= 10
   require_time = 60
 end
 
-p require_time
+reverse_of_time = 0
+
+if air_volume == 1
+  reverse_of_time = 0
+elsif air_volume == 2
+  reverse_of_time = 5
+elsif air_volume == 3
+  reverse_of_time = 15
+else
+  puts "もう一度やり直してください"
+  exit
+end
+
+temperature_diff_time = (require_time - reverse_of_time)
+
+p temperature_diff_time
+
+# input_lines = readlines
+
+# room = input_lines[0].to_i
+# air_conditioner = input_lines[1].to_i
+# air_flow = input_lines[2].to_i
+
+# temperature_diff = (room - air_conditioner).abs
+
+# require_time = 0
+
+# if temperature_diff < 5
+#     require_time = 15
+# elsif temperature_diff >= 5 && temperature_diff < 10 then
+#     require_time = 30
+# elsif temperature_diff >= 10  then
+#     require_time = 60
+# end
+
+# case air_flow
+# when 1 then
+#   require_time
+# when 2 then
+#   require_time = require_time - 5
+# when 3 then
+#   require_time = require_time - 10
+# else
+#   p "風量の値は1~3にしてください"
+#   exit
+# end
+
+# p require_time
+
